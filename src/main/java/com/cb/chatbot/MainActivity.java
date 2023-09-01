c9d4ea1e5cde1745f5e91407416c38e871606fc2
boluakindele1 <120821391+boluakindele1@users.noreply.github.com>
2023-09-01 12:41:46 -0400

    Update MainActivity.java

--- src/main/java/com/cb/chatbot/MainActivity.java
+++ src/main/java/com/cb/chatbot/MainActivity.java
@@ -102,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
         RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization","Bearer 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")
+                .header("Authorization","Bearer APiKey")
                 .post(body)
                 .build();
 
